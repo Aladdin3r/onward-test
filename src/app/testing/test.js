01334ead0340ca2cc226abd9da436ea0ab05378f
Rawan Aladdin <raladdin@my.bcit.ca>
2024-10-18 19:03:45 -0700

    empty folders not showing

--- src/app/testing/test.js
+++ src/app/testing/test.js
@@ -1,10 +1,7 @@
 export default function Test() {
     return (
         <>
-            <h1>Heading 1 Test</h1>
-            <h2>Heading 2 Test</h2>
-            <h3>Heading 3 Test</h3>
-            <p>Paragraph Body Text Test</p>
+            
         </>
     )
 }
